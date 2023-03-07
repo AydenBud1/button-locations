8a86c565f66b05fc687b088fc6c738e8e44e2e27
Ayden Budish <ab44990@ALA.local>
2023-03-07 08:25:58 -0700

    final

---
+++ Assets/java.js
@@ -0,0 +1,41 @@
+function Index1(){
+    document.getElementById("text").style.color = "blue";
+}
+function Index2(){
+    document.getElementById("text").style.color = "red";
+}
+function Index3(){
+    document.getElementById("text").style.color = "black";
+}
+function Aboutme1() {
+    document.getElementById("lore").style.visibility = "hidden";
+}
+function Aboutme2() {
+    document.getElementById("lore").style.visibility = "visible";
+}
+function Aboutme3() {
+    document.getElementById("test1").style.color = "red";
+    document.getElementById("test2").style.color = "green";
+    document.getElementById("test3").style.color = "blue";
+}
+function location1(){
+    document.getElementById("text").innerHTML = "Nunc semper eget mi quis malesuada. In hac habitasse platea dictumst. Nullam tristique orci quis tellus volutpat, eget blandit libero commodo. Morbi ornare luctus lacus, a pharetra metus lobortis nec. Mauris iaculis quam vitae ullamcorper lacinia. Aenean tincidunt ac dolor nec commodo. Aliquam pharetra nulla quis quam suscipit, ac eleifend neque hendrerit. Phasellus vehicula, lectus a facilisis tincidunt, erat mi facilisis nisl, nec pretium elit leo congue massa."
+}
+function location2(){
+    document.getElementById("text").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida nibh rhoncus enim molestie lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ut ex vitae diam venenatis tincidunt quis commodo ante. Nunc vestibulum ultrices luctus. Donec a leo purus. Donec volutpat sollicitudin sem, non pharetra magna rhoncus ut. Sed felis elit, consectetur ut porta ut, condimentum at massa. Suspendisse tempus sed arcu at volutpat. Cras convallis urna fringilla luctus congue. Fusce imperdiet dui volutpat nisi dignissim venenatis quis pellentesque tortor. Mauris scelerisque tincidunt fermentum. Curabitur mauris nisi, egestas sed tempor sed, rutrum eu metus. Phasellus eget turpis id eros porta lobortis vel eu nunc. Suspendisse interdum sollicitudin purus, at aliquam ante fermentum nec. Ut massa ligula, pretium a efficitur ac, eleifend vitae felis. Vivamus tempor enim orci, non porttitor nulla ornare sed."
+}
+function location3() {
+    document.getElementById("test4").style.backgroundColor = "red";
+    document.getElementById("test5").style.backgroundColor = "green";
+    document.getElementById("test6").style.backgroundColor = "blue";
+}
+function goods1() {
+    document.getElementById("text").style.fontFamily="serif"
+}
+
+function goods2() {
+    document.getElementById("text").style.fontFamily="SansSerif"
+}
+function goods3() {
+    document.getElementById("text").style.fontFamily="Monospaced"
+}
